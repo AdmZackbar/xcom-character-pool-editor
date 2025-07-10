b1942bdd83634511e78dc07a295036ccab0ddef6
Zach Wassynger <zach.wassynger@gmail.com>
2025-07-10 16:06:25 -0500

    feat: added detail subviews for appearances in CharPoolView
    Added AppearanceBodyView.fxml, AppearanceHeadView.fxml, and AppearanceWeaponView.fxml, along with implementations in CharPoolView.

--- src/main/java/com/wassynger/CharPoolView.java
+++ src/main/java/com/wassynger/CharPoolView.java
@@ -1,8 +1,6 @@
 package com.wassynger;
 
-import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.ObjectProperty;
@@ -16,9 +14,9 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 
@@ -28,11 +26,14 @@ public class CharPoolView extends BorderPane
 {
    private final ObjectProperty<CharacterPool> charPool;
    private final StackPane viewPlaceholder;
+   private final HeadView headView;
+   private final BodyView bodyView;
+   private final WeaponView weaponView;
 
    @FXML
    private GridPane viewChar;
    @FXML
-   private BorderPane viewAppDetail;
+   private ScrollPane viewAppDetail;
    @FXML
    private ListView<Character> listChar;
    @FXML
@@ -84,6 +85,9 @@ public class CharPoolView extends BorderPane
    {
       this.charPool = new SimpleObjectProperty<>(this, "characterPool");
       this.viewPlaceholder = new StackPane(new Label("No Character Selected"));
+      this.headView = new HeadView();
+      this.bodyView = new BodyView();
+      this.weaponView = new WeaponView();
       FxUtilities.load("CharPoolView.fxml", this);
    }
 
@@ -94,6 +98,13 @@ public class CharPoolView extends BorderPane
       centerProperty().bind(Bindings.when(listChar.getSelectionModel().selectedItemProperty().isNotNull())
             .then((Node) viewChar)
             .otherwise(viewPlaceholder));
+
+      segButtonEdit.getToggleGroup()
+            .selectedToggleProperty()
+            .addListener((obs, old, newValue) -> onSelectedAppearanceToggleChanged(old, newValue));
+      viewAppDetail.contentProperty()
+            .bind(Bindings.createObjectBinding(this::computeAppearanceDetail,
+                  segButtonEdit.getToggleGroup().selectedToggleProperty()));
 
       listChar.setCellFactory(list -> new FormattedListCell<>(this::formatCharacter));
       listChar.getSelectionModel()
@@ -106,6 +117,31 @@ public class CharPoolView extends BorderPane
       cBoxAttitude.setCellFactory(list -> new FormattedListCell<>(StaticEnum::getLocalizedString));
       cBoxAttitude.setButtonCell(new FormattedListCell<>(StaticEnum::getLocalizedString));
       cBoxAttitude.getItems().addAll(Personality.values());
+   }
+
+   private void onSelectedAppearanceToggleChanged(Toggle old, Toggle newValue)
+   {
+      if (newValue == null)
+      {
+         segButtonEdit.getToggleGroup().selectToggle(old);
+      }
+   }
+
+   private Node computeAppearanceDetail()
+   {
+      if (segButtonEdit.getToggleGroup().getSelectedToggle() == buttonHead)
+      {
+         return headView;
+      }
+      else if (segButtonEdit.getToggleGroup().getSelectedToggle() == buttonBody)
+      {
+         return bodyView;
+      }
+      else if (segButtonEdit.getToggleGroup().getSelectedToggle() == buttonWeapon)
+      {
+         return weaponView;
+      }
+      return null;
    }
 
    private String formatCharacter(Character c)
@@ -131,7 +167,6 @@ public class CharPoolView extends BorderPane
    {
       if (newValue == null)
       {
-         viewAppDetail.setCenter(null);
          return;
       }
       fieldFName.setText(newValue.get(CharacterProperty.FIRST_NAME));
@@ -153,10 +188,37 @@ public class CharPoolView extends BorderPane
       newValue.getAppearanceEnum(AppearanceProperty.GENDER, Gender.class)
             .map(g -> g == Gender.MALE ? buttonMale : buttonFemale)
             .ifPresent(b -> b.setSelected(true));
-      ScrollPane scrollPane = new ScrollPane(createView(newValue.getProperties(CharacterProperty.APPEARANCE)));
-      scrollPane.setFitToWidth(true);
-      scrollPane.setFitToHeight(true);
-      viewAppDetail.setCenter(scrollPane);
+      // Head subview
+      setCBoxValue(headView.cBoxHelmet, newValue.get(AppearanceProperty.HELMET));
+      setCBoxValue(headView.cBoxHead, newValue.get(AppearanceProperty.HEAD));
+      setCBoxValue(headView.cBoxSkinColor, newValue.get(AppearanceProperty.SKIN_COLOR));
+      setCBoxValue(headView.cBoxEyeColor, newValue.get(AppearanceProperty.EYE_COLOR));
+      setCBoxValue(headView.cBoxScar, newValue.get(AppearanceProperty.SCARS));
+      setCBoxValue(headView.cBoxPaint, newValue.get(AppearanceProperty.FACE_PAINT));
+      setCBoxValue(headView.cBoxHair, newValue.get(AppearanceProperty.HAIRCUT));
+      setCBoxValue(headView.cBoxFaceHair, newValue.get(AppearanceProperty.FACIAL_HAIR));
+      setCBoxValue(headView.cBoxHairColor, newValue.get(AppearanceProperty.HAIR_COLOR));
+      setCBoxValue(headView.cBoxPropUpper, newValue.get(AppearanceProperty.FACE_PROP_UPPER));
+      setCBoxValue(headView.cBoxPropLower, newValue.get(AppearanceProperty.FACE_PROP_LOWER));
+      // Body subview
+      setCBoxValue(bodyView.cBoxTorso, newValue.get(AppearanceProperty.TORSO));
+      setCBoxValue(bodyView.cBoxTorsoDeco, newValue.get(AppearanceProperty.TORSO_DECO));
+      setCBoxValue(bodyView.cBoxTorsoUnder, newValue.get(AppearanceProperty.TORSO_UNDERLAY));
+      setCBoxValue(bodyView.cBoxArms, newValue.get(AppearanceProperty.ARMS));
+      setCBoxValue(bodyView.cBoxArmsUnder, newValue.get(AppearanceProperty.ARMS_UNDERLAY));
+      setCBoxValue(bodyView.cBoxArmLeft, newValue.get(AppearanceProperty.LEFT_ARM));
+      setCBoxValue(bodyView.cBoxForearmLeft, newValue.get(AppearanceProperty.LEFT_FOREARM));
+      setCBoxValue(bodyView.cBoxArmLeftDeco, newValue.get(AppearanceProperty.LEFT_ARM_DECO));
+      setCBoxValue(bodyView.cBoxArmRight, newValue.get(AppearanceProperty.RIGHT_ARM));
+      setCBoxValue(bodyView.cBoxForearmRight, newValue.get(AppearanceProperty.RIGHT_FOREARM));
+      setCBoxValue(bodyView.cBoxArmRightDeco, newValue.get(AppearanceProperty.RIGHT_ARM_DECO));
+      setCBoxValue(bodyView.cBoxLegs, newValue.get(AppearanceProperty.LEGS));
+      setCBoxValue(bodyView.cBoxLegsUnder, newValue.get(AppearanceProperty.LEGS_UNDERLAY));
+      setCBoxValue(bodyView.cBoxThighs, newValue.get(AppearanceProperty.THIGHS));
+      setCBoxValue(bodyView.cBoxShins, newValue.get(AppearanceProperty.SHINS));
+      // Weapon subview
+      setCBoxValue(weaponView.cBoxTint, newValue.get(AppearanceProperty.WEAPON_TINT));
+      setCBoxValue(weaponView.cBoxPattern, newValue.get(AppearanceProperty.WEAPON_PATTERN));
    }
 
    private String getNickname(Character character)
@@ -174,32 +236,91 @@ public class CharPoolView extends BorderPane
       cBox.getSelectionModel().selectFirst();
    }
 
-   private GridPane createView(List<Property> properties)
-   {
-      GridPane detailView = new GridPane();
-      detailView.setHgap(4.0);
-      detailView.setVgap(4.0);
-      ColumnConstraints col1 = new ColumnConstraints();
-      col1.setMinWidth(USE_PREF_SIZE);
-      detailView.getColumnConstraints().add(col1);
-      int i = 0;
-      for (Property property : properties)
-      {
-         if (property.getType() == Property.Type.STRUCT)
-         {
-            detailView.addRow(i++, new Label(property.getName()), createView(
-                  ((List<?>) property.getData()).stream().map(Property.class::cast).collect(Collectors.toList())));
-         }
-         else
-         {
-            detailView.addRow(i++, new Label(property.getName()), new Label(Objects.toString(property.getData())));
-         }
-      }
-      return detailView;
-   }
-
    public void setCharPool(CharacterPool charPool)
    {
       this.charPool.set(charPool);
+   }
+
+   static class HeadView extends GridPane
+   {
+      @FXML
+      private ComboBox<String> cBoxHelmet;
+      @FXML
+      private ComboBox<String> cBoxHead;
+      @FXML
+      private ComboBox<String> cBoxSkinColor;
+      @FXML
+      private ComboBox<String> cBoxEyeColor;
+      @FXML
+      private ComboBox<String> cBoxScar;
+      @FXML
+      private ComboBox<String> cBoxPaint;
+      @FXML
+      private ComboBox<String> cBoxHair;
+      @FXML
+      private ComboBox<String> cBoxFaceHair;
+      @FXML
+      private ComboBox<String> cBoxHairColor;
+      @FXML
+      private ComboBox<String> cBoxPropUpper;
+      @FXML
+      private ComboBox<String> cBoxPropLower;
+
+      public HeadView()
+      {
+         FxUtilities.load("AppearanceHeadView.fxml", this);
+      }
+   }
+
+   static class BodyView extends GridPane
+   {
+      @FXML
+      private ComboBox<String> cBoxTorso;
+      @FXML
+      private ComboBox<String> cBoxTorsoDeco;
+      @FXML
+      private ComboBox<String> cBoxTorsoUnder;
+      @FXML
+      private ComboBox<String> cBoxArms;
+      @FXML
+      private ComboBox<String> cBoxArmsUnder;
+      @FXML
+      private ComboBox<String> cBoxArmLeft;
+      @FXML
+      private ComboBox<String> cBoxForearmLeft;
+      @FXML
+      private ComboBox<String> cBoxArmLeftDeco;
+      @FXML
+      private ComboBox<String> cBoxArmRight;
+      @FXML
+      private ComboBox<String> cBoxForearmRight;
+      @FXML
+      private ComboBox<String> cBoxArmRightDeco;
+      @FXML
+      private ComboBox<String> cBoxLegs;
+      @FXML
+      private ComboBox<String> cBoxLegsUnder;
+      @FXML
+      private ComboBox<String> cBoxThighs;
+      @FXML
+      private ComboBox<String> cBoxShins;
+
+      public BodyView()
+      {
+         FxUtilities.load("AppearanceBodyView.fxml", this);
+      }
+   }
+
+   static class WeaponView extends GridPane
+   {
+      @FXML
+      private ComboBox<String> cBoxTint;
+      @FXML
+      private ComboBox<String> cBoxPattern;
+
+      public WeaponView()
+      {
+         FxUtilities.load("AppearanceWeaponView.fxml", this);
+      }
    }
 }
